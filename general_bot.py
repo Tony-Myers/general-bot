e706966b5d99b0072943893c80c20abf016cdfcf
Tony-Myers <40142137+Tony-Myers@users.noreply.github.com>
2024-11-22 13:49:02 +0000

    Update general_bot.py

--- general_bot.py
+++ general_bot.py
@@ -2,15 +2,13 @@ import streamlit as st
 import openai
 from PyPDF2 import PdfReader
 
+
 # Retrieve password and OpenAI API key from Streamlit secrets
 PASSWORD = st.secrets["password"]
+OPENAI_API_KEY = st.secrets["openai_api_key"]
 
 # Initialize OpenAI API
 openai.api_key = OPENAI_API_KEY
-# Set OpenAI API key
-openai.api_key = st.secrets["openai_api_key"]
-client = openai
-
 # Password Authentication
 def check_password():
     def password_entered():
