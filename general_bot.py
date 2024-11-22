bcca7ca6a8d0525a82c03c50ab58c5789871e737
Tony-Myers <40142137+Tony-Myers@users.noreply.github.com>
2024-11-22 13:23:08 +0000

    Update general_bot.py

--- general_bot.py
+++ general_bot.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import openai
-import io
 from PyPDF2 import PdfReader
-from docx import Document
-from warnings import PendingDeprecationWarning
-
 
 # Set OpenAI API key
 openai.api_key = st.secrets["openai"]["api_key"]
-client = openai
 
 # Password Authentication
 def check_password():
@@ -20,16 +15,13 @@ def check_password():
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        # First run, show input for password.
         st.text_input("Enter password", type="password", on_change=password_entered, key="password")
         return False
     elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
         st.text_input("Enter password", type="password", on_change=password_entered, key="password")
         st.error("Password incorrect")
         return False
     else:
-        # Password correct.
         return True
 
 if check_password():
@@ -43,29 +35,12 @@ if check_password():
             text += page.extract_text()
         return text
 
-    # Function to extract text from DOCX
-    def extract_text_from_docx(file):
-        doc = Document(file)
-        text = ''
-        for para in doc.paragraphs:
-            text += para.text + '\n'
-        return text
-
-    uploaded_file = st.file_uploader("Choose a PDF or Word document", type=["pdf", "docx"])
+    uploaded_file = st.file_uploader("Choose a PDF document", type=["pdf"])
 
     if uploaded_file is not None:
-        if uploaded_file.type == "application/pdf":
-            text = extract_text_from_pdf(uploaded_file)
-        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            text = extract_text_from_docx(uploaded_file)
-        else:
-            st.error("Unsupported file type.")
-            st.stop()
-        
+        text = extract_text_from_pdf(uploaded_file)
         st.success("File uploaded and text extracted successfully.")
-        # Optionally display the extracted text
-        # st.write(text)
-        
+
         prompt = st.text_area("Enter your prompt", height=200)
 
         if st.button("Process"):
